b470b2c23f76ba18c9d23ef17f778cfe88d3db52
Jessica <jr.simon07@gmail.com>
2023-06-01 13:17:02 +0200

    sign up link functional on HP and navbar links distanced

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -1,11 +1,13 @@
 import "./css_files/HomePage.css"
+import { useNavigate } from "react-router"
 
 export default function HomePage() {
+    const navigate = useNavigate();
     return (
         <div className="homePage">
             <div className="welcomeDiv">
                 <h1>Welcome to the business cards of tomorrow!</h1>
-                <button>Sign Up!</button>
+                <button onClick={() => navigate("/api/v1/signup")}>Sign Up!</button>
             </div>
         </div>
     )
--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -2,6 +2,7 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { NavLink } from "react-router-dom";
+import './css_files/NavBar.css';
 
 function NavBar() {
     return (
@@ -10,9 +11,9 @@ function NavBar() {
                 <Container>
                     <Navbar.Brand to="/">BizLink</Navbar.Brand>
                     <Nav className="me-auto">
-                        <NavLink to="/">Home</NavLink>
-                        <NavLink to="/api/v1/signup">Sign up</NavLink>
-                        <NavLink to="/api/v1/login">Login</NavLink>
+                        <NavLink className="navbarLink" to="/">Home</NavLink>
+                        <NavLink className="navbarLink" to="/api/v1/signup">Sign up</NavLink>
+                        <NavLink className="navbarLink" to="/api/v1/login">Login</NavLink>
                     </Nav>
                 </Container>
             </Navbar>

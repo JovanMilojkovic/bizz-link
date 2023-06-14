bbe65fcde9f0c87a2228d3501d4ef9dde8e6350f
Jovan <jovan.milojkovic@gmx.net>
2023-06-14 11:43:50 +0200

    ignored files change

---
+++ frontend/src/components/Error.jsx
@@ -0,0 +1,29 @@
+import { Link } from "react-router-dom";
+
+import "./css_files/Error.css";
+export default function Error({ message }) {
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-md-12">
+                    <div className="error-template">
+                        <h1>Oops!</h1>
+                        <h2>404 Not Found</h2>
+                        <div className="error-details">
+                            <b>{message}</b>
+                        </div>
+                        <div className="error-actions">
+                            <Link
+                                to="/api/v1/signup/"
+                                className="btn btn-primary btn-lg"
+                            >
+                                <span className="glyphicon glyphicon-home"></span>
+                                Back to sign up
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}

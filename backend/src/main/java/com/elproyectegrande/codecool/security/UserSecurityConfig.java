7e8e070367980b06bcd8f591daf70fcb91d7abd4
Jovan <jovan.milojkovic@gmx.net>
2023-06-14 14:00:35 +0200

    update gitignore

--- backend/src/main/java/com/elproyectegrande/codecool/security/UserSecurityConfig.java
+++ backend/src/main/java/com/elproyectegrande/codecool/security/UserSecurityConfig.java
@@ -4,28 +4,25 @@ package com.elproyectegrande.codecool.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
-import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-
-import javax.sql.DataSource;
 
 @Configuration
 public class UserSecurityConfig {
 
-    @Bean
-    public UserDetailsManager userDetailsManager(DataSource dataSource) {
-        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
-        jdbcUserDetailsManager.setUsersByUsernameQuery("select user_id,pw,active from members where user_id=?");
-        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery("select user_id,role from roles where user_id=?");
-        return jdbcUserDetailsManager;
-    }
+//    @Bean
+//    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+//        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+//        jdbcUserDetailsManager.setUsersByUsernameQuery("select user_id,pw,active from members where user_id=?");
+//        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery("select user_id,role from roles where user_id=?");
+//        return jdbcUserDetailsManager;
+//    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(configurer ->
-                        configurer
-                                .requestMatchers("/").hasRole("EMPLOYEE")
+        http.authorizeHttpRequests(config ->
+                        config
+                                .requestMatchers("/").denyAll()
+                                .requestMatchers("/").hasRole("USER")
                                 .requestMatchers("/leaders/").hasRole("MANAGER")
                                 .requestMatchers("/systems/").hasRole("ADMIN")
                                 .anyRequest().authenticated()

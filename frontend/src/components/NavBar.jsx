719b87aef5f472d934e21ad0f82fcffeab77adda
Jovan <jovan.milojkovic@gmx.net>
2023-06-29 10:52:33 +0200

    Bootstrap added

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,22 +1,51 @@
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
 import { NavLink } from "react-router-dom";
-import './css_files/NavBar.css';
+import "./css_files/NavBar.css";
 
 function NavBar() {
     return (
         <>
-            <Navbar bg="light" variant="light">
-                <Container>
-                    <Navbar.Brand to="/">BizLink</Navbar.Brand>
-                    <Nav className="me-auto">
-                        <NavLink className="navbarLink" to="/">Home</NavLink>
-                        <NavLink className="navbarLink" to="/api/v1/signup">Sign up</NavLink>
-                        <NavLink className="navbarLink" to="/api/v1/login">Login</NavLink>
-                    </Nav>
-                </Container>
-            </Navbar>
+            <nav
+                className="navbar navbar-expand-lg navbar-light bg-light bg-dark border-bottom border-bottom-dark"
+                data-bs-theme="dark"
+            >
+                <div className="container-fluid">
+                    <NavLink className="navbar-brand" to="/">
+                        BizLink
+                    </NavLink>
+                    <button
+                        className="navbar-toggler"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#navbarNavAltMarkup"
+                        aria-controls="navbarNavAltMarkup"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                    >
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div
+                        className="collapse navbar-collapse"
+                        id="navbarNavAltMarkup"
+                    >
+                        <div className="navbar-nav ms-auto px-5">
+                            <NavLink className="nav-link" to="/api/v1/signup">
+                                Sign up
+                            </NavLink>
+                            <NavLink className="nav-link" to="/api/v1/login">
+                                Login
+                            </NavLink>
+                            <a
+                                className="nav-link disabled"
+                                href="#"
+                                tabIndex="-1"
+                                aria-disabled="true"
+                            >
+                                About
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </nav>
         </>
     );
 }

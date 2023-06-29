15f3c079975c5f87696de6b3a98d935c81334ad1
Jovan <jovan.milojkovic@gmx.net>
2023-06-29 11:02:19 +0200

    Name changed

---
+++ backend/src/main/java/com/elproyectegrande/codecool/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.elproyectegrande.codecool.repository;
+
+import com.elproyectegrande.codecool.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findUserByEmail(String email);
+
+}

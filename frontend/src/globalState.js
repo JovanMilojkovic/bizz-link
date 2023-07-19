bd978af7a9b8d9587cf9c0110304b9f248b07f95
Jovan <jovan.milojkovic@gmx.net>
2023-07-19 20:11:04 +0200

    new globalState for jwtToken

--- frontend/src/globalState.js
+++ frontend/src/globalState.js
@@ -4,12 +4,13 @@ const useGlobalState = create((set) => ({
     signupFailed: null,
     logInFailed: null,
     user: null,
-    isLoggedIn: false, 
+    isLoggedIn: false,
+    userToken: null,
     setSignupFailed: (error) => set({ signupFailed: error }),
     setLogInFailed: (error) => set({ logInFailed: error }),
     setLogInUserData: (data) => set({ user: data }),
-    setIsLoggedIn: (value) => set({ isLoggedIn: value }), 
+    setIsLoggedIn: (value) => set({ isLoggedIn: value }),
+    setUserToken: (token) => set({ userToken: token }),
 }));
 
 export default useGlobalState;
-

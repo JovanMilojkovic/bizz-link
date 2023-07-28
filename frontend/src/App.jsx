f6b216d8e6155834423f004080e431f5abb84f2e
Jovan <jovan.milojkovic@gmx.net>
2023-07-28 21:55:28 +0200

    New route for PageNotFound component and new config for security route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,35 +2,30 @@ import "./App.css";
 import HomePage from "./components/HomePage";
 import NavBar from "./components/NavBar";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useParams } from "react-router-dom";
 import Login from "./components/Login";
 import SignUp from "./components/SignUp";
 import ResetPassword from "./components/ResetPassword";
 import Error from "./components/Error";
 import Dashboard from "./components/Dashboard";
+import PageNotFound from "./components/PageNotFound";
 import { useEffect, useState } from "react";
 import useGlobalState from "./globalState";
 
 function App() {
     const jwtToken = useGlobalState((selector) => selector.userToken);
+    const jwt = localStorage.getItem("jwtToken");
+
     return (
-        <>
-            <NavBar />
-            <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/api/v1/signup" element={<SignUp />} />
-                <Route path="/api/v1/login" element={<Login />} />
-                <Route
-                    path="/api/v1/resetpassword"
-                    element={<ResetPassword />}
-                />
-                <Route path="/api/v1/error" element={<Error />} />
-                <Route
-                    path="/dashboard/*"
-                    element={jwtToken == null ? <Error /> : <Dashboard />}
-                />
-            </Routes>
-        </>
+        <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/api/v1/signup" element={<SignUp />} />
+            <Route path="/api/v1/login" element={<Login />} />
+            <Route path="/api/v1/resetpassword" element={<ResetPassword />} />
+            <Route path="/api/v1/error" element={<Error />} />
+            <Route path="/dashboard/:userId" element={<Dashboard />} />
+            <Route path="*" element={<PageNotFound />} />
+        </Routes>
     );
 }
 

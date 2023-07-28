3cc5fa186bddd1777ef882fcb48afbe6eb35c89f
Jovan <jovan.milojkovic@gmx.net>
2023-07-28 21:58:01 +0200

    AuthorizationService need to be adjusted but working for now

--- backend/src/main/java/com/elproyectegrande/codecool/service/AuthorizationService.java
+++ backend/src/main/java/com/elproyectegrande/codecool/service/AuthorizationService.java
@@ -1,35 +1,36 @@
 package com.elproyectegrande.codecool.service;
 
 import com.elproyectegrande.codecool.repository.UserRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.stereotype.Service;
 
 @Service
-@RequiredArgsConstructor
+
 public class AuthorizationService {
     private final UserRepository repository;
     private final JwtService jwtService;
-    private final AuthenticationManager authenticationManager;
 
-    public ResponseEntity<?> authorize(String token) {
-        String username = jwtService.extractUsername(token);
-        var user = repository.findUserByUsername(username)
-                .orElseThrow();
-        if(user!=null){
-            return new ResponseEntity<>( HttpStatusCode.valueOf(200));
-        } else {
-            return new ResponseEntity<>(HttpStatusCode.valueOf(400));
-        }
-//        return AuthenticationResponse.builder()
-//                .email(request.getEmail())
-//                .username(user.getUsername())
-//                .role(user.getRole())// Add the username to the response
-//                .token(jwtToken)
-//                .build();
+    public AuthorizationService(UserRepository repository, JwtService jwtService) {
+        this.repository = repository;
+        this.jwtService = jwtService;
 
     }
 
-}
+    public ResponseEntity<?> authorize(String token, String id) {
+        String username = jwtService.extractUsername(token);
+        System.out.println(username);
+        System.out.println(id);
+        if (id.equals(username.toLowerCase())) {
+//            var user = repository.findUserByUsername(username)
+//                    .orElseThrow(() -> new UsernameNotFoundException("user not found " + username));
+
+            return new ResponseEntity<>(HttpStatusCode.valueOf(200));
+        } else {
+                return new ResponseEntity<>(HttpStatusCode.valueOf(401));
+            }
+
+        }
+
+    }
+

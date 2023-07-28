d28fe2390c9ab2c753980c344c2392c34c889587
Jovan <jovan.milojkovic@gmx.net>
2023-07-28 22:00:23 +0200

    UsernameNotFoundException, X removed from Authorization header

--- backend/src/main/java/com/elproyectegrande/codecool/filter/JwtAuthenticationFilter.java
+++ backend/src/main/java/com/elproyectegrande/codecool/filter/JwtAuthenticationFilter.java
@@ -1,5 +1,6 @@
 package com.elproyectegrande.codecool.filter;
 
+import com.elproyectegrande.codecool.repository.UserRepository;
 import com.elproyectegrande.codecool.service.JwtService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -11,6 +12,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -23,18 +25,16 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtService jwtService;
     private final UserDetailsService userDetailsService;
+    private final UserRepository userRepository;
 
-//    @Override
-//    protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
-//        return new AntPathMatcher().match("/dashboard", request.getServletPath());
-//    }
 
     @Override
     protected void doFilterInternal(
             @NonNull HttpServletRequest request,
             @NonNull HttpServletResponse response,
             @NonNull FilterChain filterChain) throws ServletException, IOException {
-        final String authHeader = request.getHeader("X-Authorization");
+        final String authHeader = request.getHeader("Authorization");
+        System.out.println(authHeader);
         final String jwt;
         final String userName;
         if (authHeader == null || !authHeader.startsWith("Bearer ")) {
@@ -45,8 +45,9 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         jwt = authHeader.substring(7);
         userName = jwtService.extractUsername(jwt);
         if (userName != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-            UserDetails userDetails = userDetailsService.loadUserByUsername(userName);
-            System.out.println(userDetails);
+            UserDetails userDetails = userRepository.findUserByUsername(userName)
+                    .orElseThrow(() -> new UsernameNotFoundException("User not found" + userName));
+
             if (jwtService.isTokenValid(jwt, userDetails)) {
                 UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
                         userDetails,

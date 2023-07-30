a581f7b2d7f0a894c1ae32003188cc8a02ed0b15
Jovan <jovan.milojkovic@gmx.net>
2023-07-30 03:23:15 +0200

    Code cleaning

--- backend/src/main/java/com/elproyectegrande/codecool/security/SecurityConfiguration.java
+++ backend/src/main/java/com/elproyectegrande/codecool/security/SecurityConfiguration.java
@@ -5,7 +5,6 @@ import com.elproyectegrande.codecool.filter.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -24,7 +23,6 @@ import java.util.Collections;
 public class SecurityConfiguration {
 
     private final JwtAuthenticationFilter jwtAuthFilter;
-    private final AuthenticationProvider authenticationProvider;
 
     @Bean
     CorsConfigurationSource corsConfigurationSource() {

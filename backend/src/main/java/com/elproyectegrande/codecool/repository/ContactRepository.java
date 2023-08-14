ff31161b74c6e740aa62b79370895fc3cc9f1932
Jovan <jovan.milojkovic@gmx.net>
2023-08-14 02:07:04 +0200

    Interface find Query changed

--- backend/src/main/java/com/elproyectegrande/codecool/repository/ContactRepository.java
+++ backend/src/main/java/com/elproyectegrande/codecool/repository/ContactRepository.java
@@ -4,8 +4,9 @@ import com.elproyectegrande.codecool.model.Contact;
 import com.elproyectegrande.codecool.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ContactRepository extends JpaRepository<Contact, Long> {
-    Optional<User> findByEmail(String email);
+    Optional<List<Contact>> findContactsByUser(Optional<User> user);
 }

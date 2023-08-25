cf38f7bdb349574085ea6ef6fa9445142ad940ca
Jovan <jovan.milojkovic@gmx.net>
2023-08-25 15:05:23 +0200

    Decrypting the data

--- backend/src/main/java/com/elproyectegrande/codecool/controller/QRCodeController.java
+++ backend/src/main/java/com/elproyectegrande/codecool/controller/QRCodeController.java
@@ -2,21 +2,24 @@ package com.elproyectegrande.codecool.controller;
 
 import com.elproyectegrande.codecool.auth.QRCodeResponse;
 import com.elproyectegrande.codecool.service.QRCodeService;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.util.Base64Utils;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.crypto.Cipher;
 import javax.crypto.spec.SecretKeySpec;
 import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
 
 @RestController
 @RequestMapping("/business-card")
 public class QRCodeController {
     private final QRCodeService service;
-    private String secretKey = "yourSecretKey";
 
     public QRCodeController(QRCodeService service) {
         this.service = service;
@@ -24,28 +27,39 @@ public class QRCodeController {
 
     @GetMapping("")
     @PreAuthorize("hasRole('USER')")
-    public ResponseEntity<QRCodeResponse> getBusinessCardData(@RequestParam String username){
-        return service.generate(username);
+    public ResponseEntity<QRCodeResponse> getBusinessCardData(@RequestParam String username) throws NoSuchAlgorithmException {
+        String decryptedUsername = decryptData(username.replaceAll(" ", "+"));
+        System.out.println(decryptedUsername);
+        return service.generate(decryptedUsername);
     }
 
 
-    public String decryptData(String encryptedData) {
+    public String decryptData(String encryptedUsername) throws NoSuchAlgorithmException {
         try {
-            // Convert the encrypted data from base64
-            byte[] encryptedBytes = Base64Utils.decodeFromString(encryptedData);
+            byte[] keyBytes = secretKey.getBytes("UTF-8");
+            SecretKeySpec secretKeySpec = new SecretKeySpec(keyBytes, "AES");
 
-            // Initialize the AES cipher in decryption mode
-            Cipher cipher = Cipher.getInstance("AES");
-            SecretKeySpec secretKeySpec = new SecretKeySpec(secretKey.getBytes(StandardCharsets.UTF_8), "AES");
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
             cipher.init(Cipher.DECRYPT_MODE, secretKeySpec);
 
-            // Decrypt the data
-            byte[] decryptedBytes = cipher.doFinal(encryptedBytes);
-            String decryptedData = new String(decryptedBytes, StandardCharsets.UTF_8);
+            // Base64 decode the encrypted data
+            byte[] decryptedBytes = cipher.doFinal(Base64.getDecoder().decode(encryptedUsername));
 
-            return decryptedData;
+            // Decrypt the data
+
+
+            System.out.println("Decrypted Username: " + new String(decryptedBytes,"UTF-8"));
+
+            return new String(decryptedBytes,"UTF-8");
         } catch (Exception e) {
             return "Error";
         }
+
     }
-}
+
+        public String generateKeyFromOriginalKey(String originalKey) throws NoSuchAlgorithmException {
+            MessageDigest sha = MessageDigest.getInstance("SHA-256");
+            byte[] keyBytes = sha.digest(originalKey.getBytes(StandardCharsets.UTF_8));
+            return Base64.getEncoder().encodeToString(keyBytes);
+        }
+    }

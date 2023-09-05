d544810082551e2fd7eea5411daef007efab0c0f
Jovan <jovan.milojkovic@gmx.net>
2023-09-05 19:12:25 +0200

    new query findUserById

--- backend/src/main/java/com/elproyectegrande/codecool/repository/UserRepository.java
+++ backend/src/main/java/com/elproyectegrande/codecool/repository/UserRepository.java
@@ -6,8 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, String> {
     Optional<User> findByEmail(String email);
-    Optional<User> findUserByUsernameIgnoreCase (String username);
+    Optional<User> findUserByUsernameIgnoreCase(String username);
+    Optional<User> findUserById(String userId);
 
 }

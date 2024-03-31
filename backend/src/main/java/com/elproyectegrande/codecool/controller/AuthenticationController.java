a0e068aaeec08b58cc1227e5b98083ece47cf1aa
Jovan <jovan.milojkovic@gmx.net>
2024-03-31 22:59:52 +0200

    Controller Exception

--- backend/src/main/java/com/elproyectegrande/codecool/controller/AuthenticationController.java
+++ backend/src/main/java/com/elproyectegrande/codecool/controller/AuthenticationController.java
@@ -4,6 +4,7 @@ package com.elproyectegrande.codecool.controller;
 import com.elproyectegrande.codecool.auth.AuthenticationRequest;
 import com.elproyectegrande.codecool.auth.AuthenticationResponse;
 import com.elproyectegrande.codecool.service.AuthenticationService;
+import jakarta.mail.AuthenticationFailedException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,7 +23,7 @@ public class AuthenticationController {
     @PostMapping("/login")
     public AuthenticationResponse authenticate(
             @RequestBody AuthenticationRequest request
-    ) {
+    ) throws AuthenticationFailedException {
         return service.authenticate(request);
     }
 }

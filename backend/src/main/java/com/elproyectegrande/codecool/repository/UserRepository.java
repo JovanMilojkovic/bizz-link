739a02826adcc444ca7150ca8a8007d001bbe631
Jovan <jovan.milojkovic@gmx.net>
2024-03-31 23:35:27 +0200

    finding not verified users

--- backend/src/main/java/com/elproyectegrande/codecool/repository/UserRepository.java
+++ backend/src/main/java/com/elproyectegrande/codecool/repository/UserRepository.java
@@ -3,6 +3,8 @@ package com.elproyectegrande.codecool.repository;
 import com.elproyectegrande.codecool.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 
@@ -10,5 +12,5 @@ public interface UserRepository extends JpaRepository<User, String> {
     Optional<User> findByEmail(String email);
     Optional<User> findUserByUsernameIgnoreCase(String username);
     Optional<User> findUserById(String userId);
-
+    List<User> findByIsActiveFalseAndCreationTimeBefore(LocalDateTime creationTime);
 }
